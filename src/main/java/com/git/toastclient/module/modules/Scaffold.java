fc040261fda5f5700fd925aec513022ca680ab5a
BuildTools <unconfigured@null.spigotmc.org>
2020-01-19 06:45:52 -0500

    scaffy

--- src/main/java/com/git/toastclient/module/modules/Scaffold.java
+++ src/main/java/com/git/toastclient/module/modules/Scaffold.java
@@ -32,25 +32,32 @@ public class Scaffold extends Module {
 		if (HoldBlock()) {
 			Vec3d eyes = MinecraftClient.getInstance().player.getPos()
 					.add(0.0d, MinecraftClient.getInstance().player.getEyeHeight(MinecraftClient.getInstance().player.getPose()), 0.0d);
-// MinecraftClient.getInstance().player.sendChatMessage(String.format("My eyes are at %f %f %f", eyes.x, eyes.y, eyes.z));
+// System.out.printf("My eyes are at %f %f %f\n", eyes.x, eyes.y, eyes.z);
 			for (Direction side : Direction.values()) {
-// MinecraftClient.getInstance().player.sendChatMessage(String.format("The floor is at %d %d %d", floor.getX(), floor.getY(), floor.getZ()));
-// MinecraftClient.getInstance().player.sendChatMessage(String.format("It's %s neighbor is at %d %d %d", side.getName(), neighbour.getX(), neighbour.getY(), neighbour.getZ()));
+// System.out.printf("The floor is at %d %d %d\n", floor.getX(), floor.getY(), floor.getZ());
+// System.out.printf("It's %s neighbor is at %d %d %d\n", side.getName(), neighbour.getX(), neighbour.getY(), neighbour.getZ());
 				BlockPos floor = (new BlockPos(MinecraftClient.getInstance().player).down());
 				BlockPos neighbour = floor.offset(side);
 				Direction opposite = side.getOpposite();
 				Vec3d hitVec = new Vec3d(neighbour).add(new Vec3d(opposite.getUnitVector()).multiply(0.5d));
 				if (eyes.squaredDistanceTo(new Vec3d(floor)) >= eyes.squaredDistanceTo(new Vec3d(neighbour))) continue;
 				if (eyes.squaredDistanceTo(hitVec) > 18.0625) continue;
-				if (!MinecraftClient.getInstance().player.canPlaceOn(floor, opposite, MinecraftClient.getInstance().player.getStackInHand(Hand.MAIN_HAND))) continue;
+				if (!MinecraftClient.getInstance().player.canPlaceOn(floor, side, MinecraftClient.getInstance().player.getStackInHand(Hand.MAIN_HAND))) continue;
 //				if (!MinecraftClient.getInstance().world.getBlockState(neighbour).getBlock().canPlaceAt(MinecraftClient.getInstance().world.getBlockState(neighbour), MinecraftClient.getInstance().world, floor)) continue;
-				
+				BlockState neighbourblockstate = MinecraftClient.getInstance().world.getBlockState(neighbour);
+				Block neighbourblock = neighbourblockstate.getBlock();
+				if (neighbourblockstate.isAir()) continue;
+				if (neighbourblock instanceof FluidBlock) continue;
+//				if (!MinecraftClient.getInstance().player.canPlaceOn(neighbour, opposite, MinecraftClient.getInstance().player.getMainHandStack())) {
+//					continue;
+//				}
 				{// place block
 					MinecraftClient.getInstance().interactionManager.interactBlock(
 							MinecraftClient.getInstance().player,
 							MinecraftClient.getInstance().world, Hand.MAIN_HAND,
 							new BlockHitResult(hitVec, side, neighbour.offset(opposite), true)
 						);
+ System.out.printf("Block placed at %d %d %d against the %s neighbor at %d %d %d on the %s side with hitvec %1f %1f %1f\n", floor.getX(), floor.getY(), floor.getZ(), side.getName(), neighbour.getX(), neighbour.getY(), neighbour.getZ(), opposite.getName(), hitVec.getX(), hitVec.getY(), hitVec.getZ());
 					MinecraftClient.getInstance().player.swingHand(Hand.MAIN_HAND);
 					MinecraftClient.getInstance().player.inventory.selectedSlot = holdSlot;
 					return;
